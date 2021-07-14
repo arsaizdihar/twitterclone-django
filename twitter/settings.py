f32b66766dd83060f2f4548f7d52ebb003e5f92a
arsaizdihar <arsadihar@gmail.com>
2021-07-14 20:33:04 +0700

    static files

--- twitter/settings.py
+++ twitter/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "graphql_auth",
     "django_filters",
     "corsheaders",
+    "whitenoise.runserver_nostatic",
     "users",
     "tweet",
 ]
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
@@ -173,9 +175,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
